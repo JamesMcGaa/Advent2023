1d4c89e79370288347fce348bd63eaeb1d414670
James McGaa <jemcgaa@gmail.com>
2023-12-11 00:10:24 -0500

    more 8b

--- src/main/kotlin/p8.kt
+++ src/main/kotlin/p8.kt
@@ -4,9 +4,12 @@ data class Node(
     val name: String,
     val left: String,
     val right: String
-) {
+)
 
-}
+data class Ghost(
+    var current: String,
+    val seen: HashMap<Pair<String, Long>, Long> // Node, Move -> Index
+)
 
 fun main() {
     val input = File("inputs/input8.txt").readLines()
@@ -20,16 +23,58 @@ fun main() {
     }
     println(GRAPH)
 
-    var counterB = 0
-    var currentSet = GRAPH.keys.filter { it.endsWith('A') }
-    while (currentSet.filter { !it.endsWith('Z') }.isNotEmpty()) {
-        val move = MOVES[counterB % MOVES.length]
-        if (move == 'L') {
-            currentSet = currentSet.map { GRAPH[it]!!.left }
-        } else {
-            currentSet = currentSet.map { GRAPH[it]!!.right }
+    var MATH = mutableListOf<Pair<Long, Long>>()
+    var counterB = 0L
+    var currentGhosts = GRAPH.keys.filter { it.endsWith('A') }.map { Ghost(it, hashMapOf(Pair(it, 0L) to 0L)) }.toMutableSet()
+    while (currentGhosts.isNotEmpty()) {
+        val moveIdx = (counterB % MOVES.length).toLong()
+        val move = MOVES[moveIdx.toInt()]
+        val newGhosts = mutableSetOf<Ghost>()
+        currentGhosts.forEach { ghost ->
+            val newCurrent = if (move == 'L') GRAPH[ghost.current]!!.left else GRAPH[ghost.current]!!.right
+            val newSeenEntry = Pair(newCurrent, moveIdx)
+            if (ghost.seen.contains(newSeenEntry)) { // Loop detected
+                MATH.add(Pair(counterB, counterB - ghost.seen[newSeenEntry]!!))
+                println("${counterB}, ${counterB - ghost.seen[newSeenEntry]!!}")
+            } else { // Update the ghost and keep looking for a loop
+                ghost.seen[newSeenEntry] = counterB
+                ghost.current = newCurrent
+                newGhosts.add(ghost)
+            }
         }
+        currentGhosts = newGhosts
         counterB += 1
     }
-    println(counterB)
+    println(MATH)
+
+//    var MATH = mutableListOf(
+//        Pair(12086L, 12083L),
+//        Pair(14894L, 14893L),
+//        Pair(17142L, 17141L),
+//        Pair(19952L, 19951L),
+//        Pair(20520L, 20513L),
+//        Pair(22201L, 22199L)
+//    )
+    MATH = MATH.map {
+        Pair(it.first % it.second, it.second)
+    }.toMutableList()
+    println(MATH)
+
+    var current = MATH.first().first
+    var lcm = MATH.first().second
+    MATH.remove(MATH.first())
+    while (MATH.isNotEmpty()) {
+        current += lcm
+//        println(current)
+        for (pair in MATH) {
+            if (current % pair.second == (pair.first % pair.second)) {
+                MATH.remove(pair)
+                println(MATH.size)
+                lcm *= pair.second
+            }
+        }
+    }
+    println(current)
+
+
 }
