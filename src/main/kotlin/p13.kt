03da52cd651e461491a6418289ba244b66d7a001
James McGaa <jemcgaa@gmail.com>
2023-12-13 23:59:44 -0500

    p13

--- src/main/kotlin/p13.kt
+++ src/main/kotlin/p13.kt
@@ -2,7 +2,7 @@ import java.io.File
 
 fun main() {
     val pictures = mutableListOf(Picture(mutableListOf()))
-    val lines = File("inputs/input13.txt").forEachLine {
+    File("inputs/input13.txt").forEachLine {
         if (it.isEmpty()) {
             pictures.add(Picture(mutableListOf()))
         } else {
@@ -11,54 +11,81 @@ fun main() {
     }
 
     var counterA = 0
+    var counterB = 0
     pictures.forEach {
-        counterA += it.getReflectionIndex()
+        counterA += it.getReflectionIndex(false).first()
+        counterB += it.getAltReflectionIndex()
     }
     println(counterA)
+    println(counterB)
 }
 
 data class Picture(
     val lines: MutableList<String>,
+    var altLines: MutableList<String> = mutableListOf()
 ) {
-    fun getReflectionIndex(): Int {
-        findCenterRow@for (centerRowIdx in 0 .. lines.size - 2) {
+
+    fun getAltReflectionIndex(): Int {
+        for (i in lines.indices) {
+            for (j in lines[i].indices) {
+
+                altLines = lines.toMutableList()
+                var lineToChange = lines[i].toMutableList()
+                lineToChange[j] = if (lines[i][j] == '#') '.' else '#'
+                altLines[i] = lineToChange.joinToString("") // Cant use regular toString
+
+                val new = getReflectionIndex(true)
+                val old = getReflectionIndex(false)
+                val diff = new.toSet() subtract old.toSet()
+                if (diff.isNotEmpty()) {
+                    return diff.sum()
+                }
+            }
+        }
+        throw Exception("Alternative reflect not found")
+    }
+
+    fun getReflectionIndex(useAlt: Boolean): List<Int> {
+        val indices = mutableListOf<Int>()
+        findCenterRow@ for (centerRowIdx in 0..lines.size - 2) {
             var upperIdx = centerRowIdx
             var lowerIdx = centerRowIdx + 1
             while (upperIdx >= 0 && lowerIdx <= lines.lastIndex) {
-                if (getRow(upperIdx) != getRow(lowerIdx)) {
+                if (getRow(upperIdx, useAlt) != getRow(lowerIdx, useAlt)) {
                     continue@findCenterRow
                 }
                 upperIdx -= 1
                 lowerIdx += 1
             }
-            return 100 * (centerRowIdx+1)
+            indices.add(100 * (centerRowIdx + 1))
         }
 
-        findCenterCol@for (centerColIdx in 0 .. lines[0].length - 2) {
+        findCenterCol@ for (centerColIdx in 0..lines[0].length - 2) {
             var leftIdx = centerColIdx
             var rightIdx = centerColIdx + 1
             while (leftIdx >= 0 && rightIdx <= lines[0].lastIndex) {
-                if (getCol(leftIdx) != getCol(rightIdx)) {
+                if (getCol(leftIdx, useAlt) != getCol(rightIdx, useAlt)) {
                     continue@findCenterCol
                 }
                 leftIdx -= 1
                 rightIdx += 1
             }
-            return centerColIdx+1
+            indices.add(centerColIdx + 1)
         }
 
-        return -1
+        return indices
     }
 
-    fun getCol(idx: Int): String {
+    fun getCol(idx: Int, useAlt: Boolean): String {
         var col = ""
-        for (line in lines) {
+        val selectedLines = if (useAlt) altLines else lines
+        for (line in selectedLines) {
             col += line[idx]
         }
         return col
     }
 
-    fun getRow(idx: Int): String {
-        return lines[idx]
+    fun getRow(idx: Int, useAlt: Boolean): String {
+        return if (useAlt) altLines[idx] else lines[idx]
     }
 }
