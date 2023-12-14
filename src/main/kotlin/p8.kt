8dbf4aea844a4425ec9e7da0d49d65c17793e539
James McGaa <jemcgaa@gmail.com>
2023-12-13 21:27:15 -0500

    p8b - cycles and modular equations

--- src/main/kotlin/p8.kt
+++ src/main/kotlin/p8.kt
@@ -8,7 +8,7 @@ data class Node(
 
 data class Ghost(
     var current: String,
-    val seen: HashMap<Pair<String, Long>, Long> // Node, Move -> Index
+    val seen: HashMap<Pair<String, Long>, Pair<Long, Boolean>> // Node, Move -> Index, wasTerminal
 )
 
 fun main() {
@@ -23,9 +23,12 @@ fun main() {
     }
     println("Graph: ${GRAPH}")
 
-    var MATH = mutableListOf<Pair<Long, Long>>()
+    var MATH =
+        mutableListOf<Triple<Long, Long, List<Long>>>() // Initial cycle offset, cycle len, moduli that are terminal
     var counterB = 0L
-    var currentGhosts = GRAPH.keys.filter { it.endsWith('A') }.map { Ghost(it, hashMapOf(Pair(it, 0L) to 0L)) }.toMutableSet()
+    var currentGhosts =
+        GRAPH.keys.filter { it.endsWith('A') }.map { Ghost(it, hashMapOf(Pair(it, 0L) to Pair(0L, false))) }
+            .toMutableSet()
     while (currentGhosts.isNotEmpty()) {
         val moveIdx = (counterB % MOVES.length).toLong()
         val move = MOVES[moveIdx.toInt()]
@@ -34,47 +37,64 @@ fun main() {
             val newCurrent = if (move == 'L') GRAPH[ghost.current]!!.left else GRAPH[ghost.current]!!.right
             val newSeenEntry = Pair(newCurrent, moveIdx)
             if (ghost.seen.contains(newSeenEntry) && newCurrent.endsWith('Z')) { // Loop detected
-                MATH.add(Pair(counterB, counterB - ghost.seen[newSeenEntry]!!))
-                println("Cycle Detected: ${counterB}, ${counterB - ghost.seen[newSeenEntry]!!}")
+                val firstSeenIdx = ghost.seen[newSeenEntry]!!.first
+                val moduli = ghost.seen.values.filter { it.second == true && it.first >= firstSeenIdx }
+                    .map { it.first } // Parts ending with Z after the loop started (just the moduli)(
+                MATH.add(Triple(counterB, counterB - firstSeenIdx, moduli))
             } else { // Update the ghost and keep looking for a loop
-                ghost.seen[newSeenEntry] = counterB
+                ghost.seen[newSeenEntry] = Pair(counterB, newCurrent.endsWith('Z'))
                 ghost.current = newCurrent
                 newGhosts.add(ghost)
             }
         }
         currentGhosts = newGhosts
-        counterB += 1
     }
     println("Raw Math: ${MATH}")
+    val newMath =
+        MATH.map { Triple(it.first - it.second, it.second, it.third.map { marker -> marker - (it.first - it.second) }) }
+    println("Processed Math: ${newMath}")
 
-//    var MATH = mutableListOf(
-//        Pair(12086L, 12083L),
-//        Pair(14894L, 14893L),
-//        Pair(17142L, 17141L),
-//        Pair(19952L, 19951L),
-//        Pair(20520L, 20513L),
-//        Pair(22201L, 22199L)
-//    )
-    MATH = MATH.map {
-        Pair(it.first % it.second, it.second)
-    }.toMutableList()
-    println("PROCESSED Math: ${MATH}")
-
-    var current = MATH.first().first
-    var lcm = MATH.first().second
-    MATH.remove(MATH.first())
-    while (MATH.isNotEmpty()) {
-        current += lcm
-//        println(current)
-        for (pair in MATH) {
-            if (current % pair.second == (pair.first % pair.second)) {
-                MATH.remove(pair)
-                println(MATH.size)
-                lcm *= pair.second
-            }
-        }
-    }
-    println(current)
+    println(lcm(newMath.map { it.second }))
+    /*
+     * Note that from here we could do things iteratively, however by noting that the results of the math indicate that
+     * they all move in cycles of 0 .. C_i starting from the very beginning
+     *
+     * The answer is just the LCM
+     */
 
 
+    // Naive attempt
+//    var i = newMath.map { it.first }.max().toLong() // Begin where all cycles have started
+//    outer@while (true) {
+//        i += 1L
+//        for (cycle in newMath) {
+//            if (!cycle.third.contains(i % cycle.second)) {
+//                continue@outer
+//            }
+//        }
+//        println(i)
+//        return
+//    }
 }
+
+fun gcd(a: Long, b: Long): Long {
+    var A = a
+    var B = b
+    while (B > 0) {
+        val temp = B
+        B = A % B
+        A = temp
+    }
+    return a
+}
+
+private fun lcm(a: Long, b: Long): Long {
+    return a * (b / gcd(a, b))
+}
+
+private fun lcm(input: List<Long>): Long {
+    var result = input[0]
+    for (i in 1 until input.size) result = lcm(result, input[i])
+    return result
+}
+
