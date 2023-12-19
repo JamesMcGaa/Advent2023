91f61b4dc3d62ad4f02fa40937927fd3d41d64e7
James McGaa <jemcgaa@gmail.com>
2023-12-19 10:34:30 -0500

    p17b - cool graph relaxation problem

--- src/main/kotlin/p17.kt
+++ src/main/kotlin/p17.kt
@@ -15,6 +15,9 @@ import java.io.File
  * Max size of 141*141*4*3 + 1
  */
 
+var MAX_STREAK = 3
+var MIN_TURN_MOVEMENT = 0
+
 data class State(
     val x: Int,
     val y: Int,
@@ -22,7 +25,7 @@ data class State(
     val streak: Int,
 ) {
     fun legal(board: List<String>): Boolean {
-        return board.indices.contains(x) && board[x].indices.contains(y) && streak <= 3
+        return board.indices.contains(x) && board[x].indices.contains(y) && streak <= MAX_STREAK // Part B change
     }
 }
 
@@ -30,10 +33,17 @@ data class FullState(
     val state: State,
     val heatLossSoFar: Int
 ) {
+    fun isLegalOrientationForNeighbor(orient: Orientation): Boolean {
+        if (state.streak > 0 && state.streak < MIN_TURN_MOVEMENT) { // Part B change
+            return orient == state.orientation
+        }
+        return orient != state.orientation.opposite() || state.streak == 0
+    }
+
     fun getNeighbors(board: List<String>): List<FullState> {
         val neighbors = mutableListOf<FullState>()
         for (orient in Orientation.entries) {
-            if (orient != state.orientation.opposite() || state.streak == 0) {
+            if (isLegalOrientationForNeighbor(orient)) {
                 when (orient) {
                     Orientation.LEFT -> {
                         val newStreak = if (orient != state.orientation) 1 else state.streak + 1
@@ -107,37 +117,49 @@ fun Orientation.opposite(): Orientation {
 fun main() {
     val board = File("inputs/input17.txt").readLines()
 
-    val startState = State(0, 0, Orientation.RIGHT, 0)
-    val startHeat = 0
-    val optimalMap = hashMapOf<State, Int>()
-    val stack = mutableListOf(
-        FullState(startState, startHeat)
-    )
+    var isPartB = false
+    repeat(2) {
+        if (isPartB) {
+            MAX_STREAK = 10
+            MIN_TURN_MOVEMENT = 4
+        }
+        isPartB = true
 
-    var bestSoFar = 178929
-    while (stack.isNotEmpty()) {
-//        if (stack.size % 1000 == 0) {
-//            println("${stack.size}, ${optimalMap.size}")
-//        }
-        val current = stack.removeLast()
+        val startState = State(0, 0, Orientation.RIGHT, 0)
+        val startHeat = 0
+        val optimalMap = hashMapOf<State, Int>()
+        val stack = mutableListOf(
+            FullState(startState, startHeat)
+        )
 
-        if (optimalMap.getOrDefault(current.state, bestSoFar) <= current.heatLossSoFar || bestSoFar <= current.heatLossSoFar) {
-            continue
+        var bestSoFar = 178929 // (141 * 141) * 9, basic upper bound on heat loss
+        while (stack.isNotEmpty()) {
+
+            val current = stack.removeLast()
+
+            if (optimalMap.getOrDefault(current.state, bestSoFar) <= current.heatLossSoFar || bestSoFar <= current.heatLossSoFar) {
+                continue
+            }
+
+            if (current.state.x == board.lastIndex && current.state.y == board[0].lastIndex && current.heatLossSoFar < bestSoFar) {
+                bestSoFar = current.heatLossSoFar
+            }
+
+            optimalMap[current.state] = current.heatLossSoFar
+
+            for (neighbor in current.getNeighbors(board)) {
+                stack.add(neighbor)
+            }
         }
 
-        if (current.state.x == board.lastIndex && current.state.y == board[0].lastIndex && current.heatLossSoFar < bestSoFar) {
-            println(bestSoFar)
-            bestSoFar = current.heatLossSoFar
-        }
-
-        optimalMap[current.state] = current.heatLossSoFar
-
-        for (neighbor in current.getNeighbors(board)) {
-            stack.add(neighbor)
-        }
+        println(optimalMap.filterKeys { state -> state.x == board.lastIndex && state.y == board[state.x].lastIndex }.values.min())
     }
-    println(optimalMap)
-    println(optimalMap.size)
-    println(optimalMap.filterKeys { state -> state.x == board.lastIndex && state.y == board[state.x].lastIndex })
-    println(optimalMap.filterKeys { state -> state.x == board.lastIndex && state.y == board[state.x].lastIndex }.values.min())
 }
+
+/**
+ * - One person online used complex numbers to represent direction
+ *
+ * - Another based it on heat, see how many nodes we can hit with heat = 1, then heat += 1 until we find the exit, etc
+ *
+ * - Other users considered all moves between min -> max for that direction, then only handled left and right turns, not forwards
+ */
