3b1617d70c66f5c246903e99136fa452f1781ac0
James McGaa <jemcgaa@gmail.com>
2023-12-27 10:49:28 -0600

    p22, falling block simulator

---
+++ src/main/kotlin/p25.kt
@@ -0,0 +1,28 @@
+import java.io.File
+
+data class WirePath(
+    val current: String,
+    val seen: MutableSet<String> = mutableSetOf()
+)
+fun main() {
+    val adj = mutableMapOf<String, MutableSet<String>>()
+    File("inputs/input25.txt").forEachLine {
+        val source = it.split(":")[0].trim()
+        val dests = it.split(":")[1].split(" ").map { it.trim() }.filter { it.isNotEmpty() }
+        if (!adj.contains(source)) {
+            adj[source] = mutableSetOf()
+        }
+        adj[source]!!.addAll(dests)
+        for (dest in dests) {
+            if (!adj.contains(dest)) {
+                adj[dest] = mutableSetOf()
+            }
+            adj[dest]!!.add(source)
+        }
+    }
+
+    val goodPaths = mutableListOf<MutableSet<String>>()
+
+
+
+}
